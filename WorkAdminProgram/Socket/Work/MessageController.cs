b140ef4c58a9ae8cee6127c00d7b27ee8d11a1ab
nowonbun@gmail.com <nowonbun@gmail.com>
2026-03-14 14:06:16 +0900

    Initial commit

---
+++ WorkAdminProgram/Socket/Work/MessageController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkAdminProgram
+{
+    class MessageController : ISocketWorkController
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
